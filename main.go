42a4bc6bb4ea21274b334393feb522c5c90edcfe
alidevjimmy <mybestgod1@gmail.com>
2020-07-04 09:32:51 +0430

    unit if conditions

--- main.go
+++ main.go
@@ -2,14 +2,26 @@ package main
 
 import (
 	"fmt"
-	"math"
+	"math/cmplx"
 )
 
-func swap(first, second string) (string, string) {
-	return second, first
-}
+var (
+	// ToBe is just for test
+	ToBe bool = true
+	// MaxInt is Just for test
+	MaxInt uint64     = 1<<64 - 1
+	z      complex128 = cmplx.Sqrt(-5 + 12i)
+	i      int8       = 50
+)
+
+// Pi is pi number
+const Pi float32 = 3.14
 
 func main() {
-	a, b := swap("hello", "world")
-	fmt.Println(a, b, ' ', math.Pi)
+	i := 20
+	if i == 20 {
+		fmt.Println("ok")
+	}
+
+	// fmt.Printf("%T\n", i)
 }
